56fcc5d5e8a5d1505643c32846e1888c43a52d67
jpukabeals <pukab001@umn.edu>
2023-03-15 14:18:22 -0500

    Adding and deleting files

---
+++ rallf_nir_addInTreatmentInfo.R
@@ -0,0 +1,25 @@
+
+# Add in the rallf keys to the NIR data
+
+library(tidyverse)
+
+source("rallf_keys.R",
+       local=F)
+
+# read in data
+# summarise across multiple observations of a code so there is only one observation per code
+# add in the treatment keys associated with the code plot and year. 
+
+read.csv(
+  "rallf_nirData_15Mar2023.csv"
+) %>% 
+  group_by(code) %>% 
+  # glimpse()
+  summarise(
+    across(
+      .cols = 4:22,
+      .fns = mean
+    )) %>% 
+  left_join(key3) %>%
+  left_join(key2) %>%
+  left_join(key1) -> dat
